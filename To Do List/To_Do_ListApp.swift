edb93a1bad64f33489b87c6f2ee1eda12febf8b2
Yawar Kamran <yawarkamran316@gmail.com>
2021-01-28 10:30:11 -0600

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Student on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
